69dc2b2c6886b1b8de1cfeee645a908546e27369
liam <liamcabral88@gmail.com>
2023-01-18 16:20:10 -0300

    feat: config to deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,15 +9,14 @@ require('dotenv').config();
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mssql',
+      type: 'postgres',
       host: process.env.DATABASE_HOST,
       port: Number(process.env.DATABASE_PORT),
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_DATABASE,
       entities: [Restaurant, RestaurantHours],
-      synchronize: true,
-      options: { encrypt: false }
+      synchronize: true
     }),
     RestaurantModule
   ],
